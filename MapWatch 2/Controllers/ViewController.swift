2368f2b3e10d543df23875635c680e1125a7197a
Joe Suzuki <joesuzuki516@gmail.com>
2017-10-11 19:35:26 -0400

    Firebase intergration

--- MapWatch 2/Controllers/ViewController.swift
+++ MapWatch 2/Controllers/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MapWatch 2
 //
-//  Created by Joe Suzuki on 10/8/17.
+//  Created by Joe Suzuki on 10/8/1/Users/Joesuzuki/Desktop/MapWatch 2/MapWatch 27.
 //  Copyright © 2017 JoeSuzuki. All rights reserved.
 //
 
---
+++ Pods/Target Support Files/Pods-MapWatch 2/Pods-MapWatch 2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapWatch_2 : NSObject
+@end
+@implementation PodsDummy_Pods_MapWatch_2
+@end
